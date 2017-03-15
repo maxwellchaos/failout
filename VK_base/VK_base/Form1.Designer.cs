d2e8049bb448d93ffe74ac9835f88d2caa81659c
maxwellchaos <maxwellchaos@gmail.com>
2017-03-15 11:17:54 +0300

    удалена строка меню

--- VK_base/VK_base/Form1.Designer.cs
+++ VK_base/VK_base/Form1.Designer.cs
@@ -35,10 +35,6 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.SpamButton = new System.Windows.Forms.Button();
             this.DogPatulButton = new System.Windows.Forms.Button();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.рассылкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.поискУдаленныхToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.AutoLike_button = new System.Windows.Forms.Button();
             this.autoadd = new System.Windows.Forms.CheckBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
@@ -49,16 +45,15 @@
             this.gropssercbaton = new System.Windows.Forms.Button();
             this.Laikbutton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // webBrowser1
             // 
             this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webBrowser1.Location = new System.Drawing.Point(0, 24);
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(945, 446);
+            this.webBrowser1.Size = new System.Drawing.Size(945, 470);
             this.webBrowser1.TabIndex = 0;
             this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
             // 
@@ -108,37 +103,6 @@
             this.DogPatulButton.UseVisualStyleBackColor = true;
             this.DogPatulButton.Click += new System.EventHandler(this.DogPatulButton_Click);
             // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.менюToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(945, 24);
-            this.menuStrip1.TabIndex = 6;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // менюToolStripMenuItem
-            // 
-            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.рассылкаToolStripMenuItem,
-            this.поискУдаленныхToolStripMenuItem});
-            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
-            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
-            this.менюToolStripMenuItem.Text = "Меню";
-            // 
-            // рассылкаToolStripMenuItem
-            // 
-            this.рассылкаToolStripMenuItem.Name = "рассылкаToolStripMenuItem";
-            this.рассылкаToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
-            this.рассылкаToolStripMenuItem.Text = "Рассылка";
-            // 
-            // поискУдаленныхToolStripMenuItem
-            // 
-            this.поискУдаленныхToolStripMenuItem.Name = "поискУдаленныхToolStripMenuItem";
-            this.поискУдаленныхToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
-            this.поискУдаленныхToolStripMenuItem.Text = "Поиск удаленных";
-            // 
             // AutoLike_button
             // 
             this.AutoLike_button.Location = new System.Drawing.Point(709, 124);
@@ -180,6 +144,7 @@
             // 
             this.Pirat_button.BackColor = System.Drawing.Color.SandyBrown;
             this.Pirat_button.Font = new System.Drawing.Font("News706 BT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pirat_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Pirat_button.Location = new System.Drawing.Point(711, 280);
             this.Pirat_button.Name = "Pirat_button";
             this.Pirat_button.Size = new System.Drawing.Size(184, 77);
@@ -236,14 +201,10 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.webBrowser1);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -257,10 +218,6 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button SpamButton;
         private System.Windows.Forms.Button DogPatulButton;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem рассылкаToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem поискУдаленныхToolStripMenuItem;
         private System.Windows.Forms.Button AutoLike_button;
         private System.Windows.Forms.CheckBox autoadd;
         private System.Windows.Forms.Timer timer1;
