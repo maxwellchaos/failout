b47bd6ae3940340525916e10b4201facd70f9f43
VLad199255 <vladgorin199@mail.ru>
2017-06-19 18:09:57 +0300

    Add files via upload

---
+++ VK_base/VK_base/assistant.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsApplication1
+{
+    public partial class assistant : Form
+    {
+        public string access_token;
+        public string user_id;
+        public assistant()
+        {
+            InitializeComponent();
+        }
+
+        private void assistant_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted_1(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+           
+        }
+
+        private void assistant_Load_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
